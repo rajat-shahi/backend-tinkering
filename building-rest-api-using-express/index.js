b19b3a15015a19350a83b65d931029f4602d0297
Rajat Shahi <92717102+rajat-shahi@users.noreply.github.com>
2026-04-06 07:02:14 +0530

    Implement user CRUD with file persistence
    Enable form body parsing and persistent user mutations. Change users import to a mutable variable and require fs; add middleware to parse urlencoded bodies and auto-assign an id on POST. Implement POST /api/users to append users and write MOCK_DATA.json, PATCH /users/:id to update a user and persist changes, and DELETE /users/:id to remove a user and persist. Add basic error handling/logging and minor HTML formatting cleanup.

--- building-rest-api-using-express/index.js
+++ building-rest-api-using-express/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 8001;
-const users = require("./MOCK_DATA.json");
+let users = require("./MOCK_DATA.json");
+const fs = require("fs");
+
+// using the middleware to parse the form data
+app.use(express.urlencoded({ extended: false }));
+
+// middleware to add the 'id' to the user to be inserted
+app.use((req, res, next) => {
+  if (req.path === "/api/users" && req.method === "POST") {
+    req.body.id = users.length + 1;
+  }
+  next();
+});
 
 // routes
 app.get("/", (req, res) => {
@@ -11,9 +23,9 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   // convert every element "user name" as li element
   const html = `
-        <ul> 
+        <ul>
             ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-        </ul>    
+        </ul>
     `;
 
   return res.send(html);
@@ -36,14 +48,55 @@ app
     }
   })
   .patch((req, res) => {
-    return res.send({ message: "will complete later" });
+    const updatedBody = req.body;
+    const userId = Number(req.params.id);
+
+    const userIndex = users.findIndex((user) => user.id === userId);
+    if (userIndex < users.length) {
+      updatedBody.id = users[userIndex].id;
+      users[userIndex] = updatedBody;
+
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+          throw err;
+        } else {
+          return res.send(
+            `The data sent is successfully updated for id ${users.length}`,
+          );
+        }
+      });
+    } else {
+      return res.send(`No user with the id = ${userId} found`);
+    }
   })
   .delete((req, res) => {
-    return res.send({ message: "will complete later" });
+    const userId = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === userId);
+    if (userIndex === -1) {
+      return res.send(`No user with the id = ${userId} found`);
+    } else {
+      users.splice(userId - 1, 1);
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        return res.send(
+          `The data sent is successfully deleted for id ${userId}`,
+        );
+      });
+    }
   });
 
 app.post("/api/users", (req, res) => {
-  return res.send({ message: "will complete later" });
+  const reqBody = req.body;
+  console.log(req.body.id);
+  users.push(reqBody);
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+    if (err) {
+      console.error("Error while writing to the file : ", err);
+    } else {
+      return res.send(
+        `The data sent is successfully processed and entered with id ${users.length}`,
+      );
+    }
+  });
 });
 
 app.listen(port, () => {
